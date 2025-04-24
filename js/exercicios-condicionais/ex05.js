70ecde3cf1946d7e1f5f945f97ef460a2f8502b1
Flávia Reis <flaviarster@gmail.com>
2025-04-24 08:38:38 -0300

    exercícios 04 e 05

---
+++ js/exercicios-condicionais/ex05.js
@@ -0,0 +1,17 @@
+// 5. Encontrar o dobro de um número caso ele seja positivo e o seu triplo caso seja negativo, imprimindo o resultado.
+const prompt = require("prompt-sync")()
+
+let numero = Number(prompt('Digite um número: '))
+let dobro = numero * 2
+let triplo = numero * 3
+let resultado
+
+if (numero > 0) {
+    resultado = dobro
+} else if (numero === 0) {
+    console.log('Digite um valor diferente de zero!')
+} else {
+    resultado = triplo
+}
+
+console.log(resultado)
---
+++ js/exercicios-condicionais/ex06.js
@@ -0,0 +1,3 @@
+// 6. Escreva um algoritmo que lê dois valores booleanos (lógicos) e então determina se ambos são
+// VERDADEIROS ou FALSOS.
+const prompt = require("prompt-sync")()
