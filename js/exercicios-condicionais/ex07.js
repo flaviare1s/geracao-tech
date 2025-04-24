b90ea51e7a8b1e1e96c5e88b2d77357bfa9c755b
Flávia Reis <flaviarster@gmail.com>
2025-04-24 08:57:36 -0300

    exercício 07

---
+++ js/exercicios-condicionais/ex07.js
@@ -0,0 +1,11 @@
+// 7. Faça um algoritmo que leia uma variável e some 5 caso seja par ou some 8 caso seja ímpar,
+// imprimir o resultado desta operação.
+const prompt = require("prompt-sync")()
+
+let numero = Number(prompt('Digite um número: '))
+
+if (numero % 2 === 0) {
+    console.log(numero + 5)
+} else {
+    console.log(numero + 8)
+}
--- js/repeticao.js
+++ js/repeticao.js
@@ -1,6 +1,20 @@
+// WHILE: 
 let i = 0
 
 while (i <= 10) {
     console.log(i)
     i++
 }
+
+// FOR:
+for (let i = 0; i <= 10; i+=2) {
+    console.log(i)
+}
+
+// DO WHILE:
+let c = 0
+
+do {
+    console.log(`${c} -> Sou menor que 10`)
+    c++
+} while (c < 10)
