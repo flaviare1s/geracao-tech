5f1325f4c72f06e9d7e1eb4166ea6c807ded16bb
Flávia Reis <flaviarster@gmail.com>
2025-04-24 11:05:01 -0300

    exercício 10 resolvido

--- js/exercicios-condicionais/ex10.js
+++ js/exercicios-condicionais/ex10.js
@@ -1,5 +1,5 @@
-// 10. O IMC – Indice de Massa Corporal é um critério da Organização Mundial de Saúde para dar
-// umaindicação sobre a condição de peso de uma pessoa adulta. A fórmula é IMC = peso / ( altura )2
+// 10. O IMC – Índice de Massa Corporal é um critério da Organização Mundial de Saúde para dar
+// uma indicação sobre a condição de peso de uma pessoa adulta. A fórmula é IMC = peso / ( altura )2
 // Elabore um algoritmo que leia o peso e a altura de um adulto e mostre sua condição de acordo
 // com a tabela abaixo.
 // IMC em adultos Condição
@@ -7,3 +7,18 @@
 // Entre 18,5 e 25 Peso normal
 // Entre 25 e 30 Acima do peso
 // Acima de 30 obeso
+const prompt = require("prompt-sync")()
+
+let peso = Number(prompt("Digite o seu peso: "))
+let altura = Number(prompt("Digite a sua altura (em cm): "))
+let IMC = (peso / altura ** 2).toFixed(2)
+
+if (IMC < 18.5) {
+  console.log(`Seu IMC é ${IMC} e você está abaixo do peso.`)
+} else if (IMC >= 18.5 && IMC < 25) {
+  console.log(`Seu IMC é ${IMC} e você está com o peso normal.`)
+} else if (IMC >= 25 && IMC < 30) {
+  console.log(`Seu IMC é ${IMC} e você acima do peso.`)
+} else {
+  console.log(`Seu IMC é ${IMC} e você está com obesidade.`)
+}
